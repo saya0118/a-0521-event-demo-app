4c42bf33f1242b8b4a83cefc7beba21dd212e195
Francois Polo <andasan@gmail.com>
2021-11-08 14:54:23 -0800

    added db.json

--- src/App.js
+++ src/App.js
@@ -9,11 +9,7 @@ import NewEventForm from './NewEventForm';
 function App() {
   const [showModal, setShowModal] = useState(false)
   const [showEvents, setShowEvents] = useState(true)
-  const [events, setEvents] = useState([
-    {title: 'React Dojo', id: 1},
-    {title: 'Svelte Meetup', id: 2},
-    {title: 'Remix Launch', id: 3},
-  ])
+  const [events, setEvents] = useState([])
 
   const handleClose = () => {
     setShowModal(false)
